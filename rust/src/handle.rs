3a5bcacd216b4ea2f89d1d29dc13340edf8ff5a8
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2025-06-16 17:46:31 +0100

    stuff

--- rust/src/handle.rs
+++ rust/src/handle.rs
@@ -6,13 +6,23 @@ pub struct Handle<I> {
 	_phantom: PhantomData<I>,
 }
 
+impl<I> Handle<I> {
+	pub fn try_as<T: Protocol>(&self) -> Option<&Handle<T>> {
+		None
+	}
+
+	pub fn as_raw(&self) -> isize { self.raw }
+
+	pub unsafe fn from_raw(raw: isize) -> Self {
+		Self {
+			raw, _phantom: PhantomData
+		}
+	}
+}
+
 impl<I: Protocol> Handle<I> {
 	pub fn new(path: &str, args: I::Ctor) -> crate::Result<Self> {
 		todo!()
-	}
-
-	pub fn try_as<T: Protocol>(&self) -> Option<&Handle<T>> {
-		None
 	}
 }
 
